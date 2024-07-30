0df19aeaca08a217132ac28da01bd3b77d33b20e
27qbracken09 <quinnbracken09@outlook.com>
2024-07-30 15:10:35 -0600

    Added Lift Command (Up & Down buttons)

--- 2131Y_Code_24-25/src/misc.cpp
+++ 2131Y_Code_24-25/src/misc.cpp
@@ -21,8 +21,16 @@ void update_clamp(char pnumatic_port, pros::controller_digital_e_t Controller_bu
 
 }
 
+void update_lift(int lift_port, float lift_max, pros::controller_digital_e_t Controller_button_up, pros::controller_digital_e_t Controller_button_down){
+    if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_MASTER, Controller_button_up)) pros::c::motor_move_absolute(lift_port, lift_max, 100);
+    if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_MASTER, Controller_button_down)) pros::c::motor_move_absolute(lift_port, 0, 100);
+}
+
+
 void update_mechanisms(){
     update_clamp('A', pros::E_CONTROLLER_DIGITAL_L2, pros::E_CONTROLLER_DIGITAL_L1);
-
+    update_lift(10, 200, pros::E_CONTROLLER_DIGITAL_UP, pros::E_CONTROLLER_DIGITAL_DOWN);
+    
 }
+
 
