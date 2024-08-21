7139b8b8a43ec040d539c3a97656b2fa807fd6f8
27qbracken09 <quinnbracken09@outlook.com>
2024-08-21 10:53:36 -0600

    Updated Intake controller poll command so that it works

--- 2131Y_Code_24-25/src/misc.cpp
+++ 2131Y_Code_24-25/src/misc.cpp
@@ -26,10 +26,18 @@ void update_lift(int lift_port, float lift_max, pros::controller_digital_e_t Con
     if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_MASTER, Controller_button_down)) pros::c::motor_move_absolute(lift_port, 0, 100);
 }
 
+void update_intake(int intake_port, float max_speed, pros::controller_digital_e_t Controller_Intake, pros::controller_digital_e_t Controller_Outtake){
+    if (pros::c::controller_get_digital(pros::E_CONTROLLER_MASTER, Controller_Intake)) pros::c::motor_move(intake_port, max_speed);
+    if (!pros::c::controller_get_digital(pros::E_CONTROLLER_MASTER, Controller_Intake)) pros::c::motor_move(intake_port, 0);
+   // if (!pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_MASTER, Controller_Intake)) pros::c::motor_move(intake_port, 0);
+    if (pros::c::controller_get_digital(pros::E_CONTROLLER_MASTER, Controller_Outtake)) pros::c::motor_move(intake_port, -max_speed);
+    //if (!pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_MASTER, Controller_Outtake)) pros::c::motor_move(intake_port, 0);
+}
 
 void update_mechanisms(){
     update_clamp(clamp_port, pros::E_CONTROLLER_DIGITAL_L2, pros::E_CONTROLLER_DIGITAL_L1);
     update_lift(lift_motor, 200, pros::E_CONTROLLER_DIGITAL_UP, pros::E_CONTROLLER_DIGITAL_DOWN);
+    update_intake(intake_motor, 1200, pros::E_CONTROLLER_DIGITAL_R1, pros::E_CONTROLLER_DIGITAL_R2);
     
 }
 
